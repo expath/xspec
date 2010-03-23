97138319bf1923170c24f2bc30372397c6fc9a63
fgeorges@gmail.com <fgeorges@gmail.com@7466eb41-2b56-0410-9bce-5946398f495e>
2010-03-23 20:13:01 +0000

    Simple reformating.

--- trunk/xspec.sh
+++ trunk/xspec.sh
@@ -7,13 +7,12 @@ XSPEC=$1
 
 if [ ! -f "$XSPEC" ]
 then
-   echo File not found.
-   echo Usage:
-   echo   xspec filename [coverage]
-   echo     filename should specify an XSpec document
-   echo     if coverage is specified, outputs test coverage report
-   exit 1
-   # exit with code 1 is error exit
+    echo File not found.
+    echo Usage:
+    echo   xspec filename [coverage]
+    echo     filename should specify an XSpec document
+    echo     if coverage is specified, outputs test coverage report
+    exit 1
 fi
 
 COVERAGE=$2
@@ -21,43 +20,48 @@ COVERAGE=$2
 TEST_DIR=$(dirname "$XSPEC")/xspec
 TARGET_FILE_NAME=$(basename "$XSPEC" | sed 's:\...*$::')
 
-TEST_STYLESHEET=$TEST_DIR/$TARGET_FILE_NAME.xsl
+COMPILED=$TEST_DIR/$TARGET_FILE_NAME.xsl
 COVERAGE_XML=$TEST_DIR/$TARGET_FILE_NAME-coverage.xml
 COVERAGE_HTML=$TEST_DIR/$TARGET_FILE_NAME-coverage.html
 RESULT=$TEST_DIR/$TARGET_FILE_NAME-result.xml
 HTML=$TEST_DIR/$TARGET_FILE_NAME-result.html
+COVERAGE_CLASS=com.jenitennison.xslt.tests.XSLTCoverageTraceListener
 
 if [ ! -d "$TEST_DIR" ]
 then
- echo "Creating XSpec Directory at $TEST_DIR..."
- mkdir "$TEST_DIR"
- echo
+    echo "Creating XSpec Directory at $TEST_DIR..."
+    mkdir "$TEST_DIR"
+    echo
 fi 
 
 echo "Creating Test Stylesheet..."
-java net.sf.saxon.Transform -o:"$TEST_STYLESHEET" -s:"$XSPEC" -xsl:"$XSPEC_HOME/generate-xspec-tests.xsl"
+java net.sf.saxon.Transform -o:"$COMPILED" -s:"$XSPEC" \
+    -xsl:"$XSPEC_HOME/generate-xspec-tests.xsl"
 echo
 
 echo "Running Tests..."
 if test "$COVERAGE" = "coverage" 
 then 
- echo "Collecting test coverage data; suppressing progress report..."
- java net.sf.saxon.Transform -T:com.jenitennison.xslt.tests.XSLTCoverageTraceListener	\
-     -o:"$RESULT" -s:"$XSPEC" -xsl:"$TEST_STYLESHEET" -it:{http://www.jenitennison.com/xslt/xspec}main 2> "$COVERAGE_XML"
+    echo "Collecting test coverage data; suppressing progress report..."
+    java net.sf.saxon.Transform -T:$COVERAGE_CLASS \
+        -o:"$RESULT" -s:"$XSPEC" -xsl:"$COMPILED" \
+        -it:{http://www.jenitennison.com/xslt/xspec}main 2> "$COVERAGE_XML"
 else
- java net.sf.saxon.Transform -o:"$RESULT" -s:"$XSPEC" -xsl:"$TEST_STYLESHEET" -it:{http://www.jenitennison.com/xslt/xspec}main
+    java net.sf.saxon.Transform -o:"$RESULT" -s:"$XSPEC" -xsl:"$COMPILED" \
+        -it:{http://www.jenitennison.com/xslt/xspec}main
 fi
 
 echo
 echo "Formatting Report..."
-java net.sf.saxon.Transform -o:"$HTML" -s:"$RESULT" -xsl:"$XSPEC_HOME/format-xspec-report.xsl"
+java net.sf.saxon.Transform -o:"$HTML" -s:"$RESULT" \
+    -xsl:"$XSPEC_HOME/format-xspec-report.xsl"
 if test "$COVERAGE" = "coverage" 
-then 
- java net.sf.saxon.Transform -l:on -o:"$COVERAGE_HTML" -s:"$COVERAGE_XML" -xsl:"$XSPEC_HOME/coverage-report.xsl" "tests=$XSPEC"
- open "$COVERAGE_HTML"
+then
+    java net.sf.saxon.Transform -l:on -o:"$COVERAGE_HTML" -s:"$COVERAGE_XML" \
+        -xsl:"$XSPEC_HOME/coverage-report.xsl" "tests=$XSPEC"
+    open "$COVERAGE_HTML"
 else
- open "$HTML"
+    open "$HTML"
 fi
 
 echo "Done."
-exit 0
