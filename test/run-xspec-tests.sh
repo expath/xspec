6dc9376b85f6749014e62d747976b56aac92b92a
Sandro Cirulli <sandro.cirulli@oup.com>
2016-08-21 17:58:26 +0100

    add documentation

--- test/run-xspec-tests.sh
+++ test/run-xspec-tests.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
-# run it inside test directory
+#===============================================================================
+#
+#         USAGE:  ./run-xspec.tests.sh 
+#         
+#   DESCRIPTION:  This script is a runner for XSpec test suite inside test directory
+#		  it runs all the tests with extension .xspec in the test directory
+#		  it outputs stdout and stderr into a file result.log
+#	          it greps for failing tests in the log file
+#		  it returns no output if the tests are successful
+#		  or an error message if there are failing tests
+#
+#         INPUT:  N/A
+#
+#        OUTPUT:  Error message if there are failing tests
+#	          A log file is created at result.log
+#
+#  DEPENDENCIES:  This script must be run from the current directory (i.e. the test directory)
+#
+#        AUTHOR:  Sandro Cirulli, github.com/cirulls
+#
+#       LICENSE:  MIT License
+#
+#===============================================================================
 for xspectest in *.xspec; 
 	do ../bin/xspec.sh $xspectest &> result.log; 
 	if grep -q ".*failed:\s[1-9]" result.log; 
